9f3f66f721e1e593b213dc440764d914969c9b31
Anton Spivak <hexkpz@gmail.com>
2025-11-29 19:38:33 +0400

    Fix `swift-bip` minimum version

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-crypto.git", from: "3.10.0"),
 
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.5.1"),
-        .package(url: "https://github.com/hexkpz/swift-bip.git", from: "0.0.1"),
+        .package(url: "https://github.com/hexkpz/swift-bip.git", from: "0.0.2"),
     ],
     targets: [
         .target(
